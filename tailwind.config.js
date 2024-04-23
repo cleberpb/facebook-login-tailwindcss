f74d6e2a7a0bf741b130378ad701b4abdd21bf4f
cleberpb <36317652+cleberpb@users.noreply.github.com>
2024-04-23 14:14:03 -0300

    Finalizando profeto

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     extend: {
       colors: {
         'blue-facebook': '#1877f2',
-        'gray-facebook': '#f0f2f5'
+        'gray-facebook': '#f0f2f5',
+        'green-facebook': '#36a420',
+      },
+      spacing: {
+        '580': '580px',
+        '380':'380px',
       }
     },
   },
