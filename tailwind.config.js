b1654d8cd792ef3f22952c9c4bea1d325d55856a
cleberpb <36317652+cleberpb@users.noreply.github.com>
2024-04-23 10:45:31 -0300

    Configurando tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'blue-facebook': '#1877f2'
+      }
+    },
+  },
+  plugins: [],
+}
+
